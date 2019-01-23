5ece8f936804ab60884e932def0d3c4c1a912762
Ewelina Skwierawska <ewelinaskwierawska@Ewelinas-MacBook-Air.local>
2019-01-23 20:46:51 +0100

    email and number of guests added

--- api/models/reservationListModel.js
+++ api/models/reservationListModel.js
@@ -3,13 +3,17 @@ let reservations = [
     id: 1,
     reservingUser: "John",
     reservingFrom: "Monday",
-    reservingTo: "Wednesday"
+    reservingTo: "Wednesday",
+    email: "test@domain.com",
+    nrOfGuests: 2
   },
   {
     id: 2,
     reservingUser: "Jane",
     reservingFrom: "Monday",
-    reservingTo: "Wednesday"
+    reservingTo: "Wednesday",
+    email: "test2@domain.com",
+    nrOfGuests: 4
   }
 ];
 
@@ -18,9 +22,16 @@ exports.findAll = async function() {
 };
 
 exports.add = function(record) {
+  const maxId = reservations
+    .map(user => user.id)
+    .reduce((acc, next) => (acc > next ? acc : next));
+
+  record.id = maxId + 1;
   reservations.push(record);
 };
 
 exports.deleteById = function(idToRemove) {
-    reservations = reservations.filter(reservation => reservation.id !== idToRemove);
+  reservations = reservations.filter(
+    reservation => reservation.id !== idToRemove
+  );
 };
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3001;
 
 bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function(req, response, next) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+  next();
+});
 
 var routes = require("./api/routes/reservationListRoute");
 routes(app);
